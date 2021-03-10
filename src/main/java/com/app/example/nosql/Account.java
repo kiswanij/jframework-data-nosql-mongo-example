10a38d36b2aa224d448c46577d02cd5c66f96063
Jalal Kiswani <kiswanij@gmail.com>
2021-03-10 10:07:20 +0200

    Initial version

---
+++ src/main/java/com/app/example/nosql/Account.java
@@ -0,0 +1,32 @@
+package com.app.example.nosql;
+
+public class Account {
+	int id;
+	String name;
+	double balance;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+}
